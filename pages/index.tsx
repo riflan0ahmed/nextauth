970c4ebf4e422e544b5c118d051b2236c99cbc58
riflan <riflan0ahmed@gmail.com>
2022-01-20 08:34:52 +0530

    refactor: working on authentication

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from "next";
 import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
   const router = useRouter();
+
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.replace("/api/auth/signin");
+    },
+  });
 
   return (
     <>
