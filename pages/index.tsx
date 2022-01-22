f09741bba5d6762ee805f7040411d69652f04763
riflan <riflan0ahmed@gmail.com>
2022-01-22 23:07:13 +0530

    refactor: working on authentication

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import type { NextPage } from "next";
 import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 import { Router, useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
   const router = useRouter();
-  const { status, data: session } = useSession();
+  const { status, data: session } = useSession({ required: true });
 
   // const { status, data: session } = useSession({
   //   required: true,
@@ -26,14 +27,14 @@ const Home: NextPage = () => {
   // ) : null;
 
   return (
-    <>
+    <div className="h-screen bg-gray-500">
       <Link href={"/protected"}>
-        <a>Protected Page</a>
+        <a className="text-2xl">Protected Page</a>
       </Link>
       <br />
       Signed in as {session?.user?.email} <br /> <br />
       <button onClick={() => signOut()}>Sign out</button>
-    </>
+    </div>
   );
 };
 
